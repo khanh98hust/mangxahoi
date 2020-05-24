7c4ef6e43427433bb9fa8b53b50e39fafeb65f44
Ngoc Khanh <ngockhanh98tb@gmail.com>
2020-05-25 01:22:02 +0700

    tao layout cho guest

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/', function () {
+        return view('layouts.guest');
+    });
 });
