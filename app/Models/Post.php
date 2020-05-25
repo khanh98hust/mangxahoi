5ffd285063cefc684374ac190a7318266724431d
Phuong Hoa <meomincute98@gmail.com>
2020-05-25 11:04:30 +0700

    fix chuc nang post bai

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -33,4 +33,35 @@ class Post extends Model
     public function likes(){
         return $this->hasMany('App\Models\PostLike', 'post_id', 'id');
     }
+
+    public function getLikeCount(){
+        if ($this->like_count == null){
+            $this->like_count = $this->likes()->count();
+        }
+        return $this->like_count;
+    }
+
+    public function getCommentCount(){
+        if ($this->comment_count == null){
+            $this->comment_count = $this->comments()->count();
+        }
+        return $this->comment_count;
+    }
+
+    public function checkOwner($user_id){
+        if ($this->user_id == $user_id)return true;
+        return false;
+    }
+
+    public function hasImage(){
+        return $this->has_image;
+    }
+
+    public function checkLike($user_id){
+        if ($this->likes()->where('like_user_id', $user_id)->get()->first()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
